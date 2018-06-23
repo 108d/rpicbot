3dbe694de2a9e7f676df8b8f795ce8a7e7313958
108Daniel <lidaniel916@gmail.com>
2018-06-23 15:12:32 -0700

    yea

--- rpicbot.py
+++ rpicbot.py
@@ -1,3 +1,10 @@
 import praw
 
 reddit = praw.Reddit('bot');
+subreddit = reddit.subreddit('conspiracy');
+
+for submission in subreddit.hot(limit=5):
+    print("Title: ", submission.title)
+    print("Text: ", submission.selftext)
+    print("Score: ", submission.score)
+    print("---------------------------------\n")
